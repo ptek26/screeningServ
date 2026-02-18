bf0b064eefec0a019345c0273600c174543bebfc
AkshayKappala <akshaykappala98@gmail.com>
2026-02-18 13:56:16 -0600

    chore: initial project scaffold — Spring Boot 4.0.2 with JPA, Liquibase, Web

---
+++ src/main/java/com/ptek/screeningServ/ScreeningServApplication.java
@@ -0,0 +1,13 @@
+package com.ptek.screeningServ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreeningServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreeningServApplication.class, args);
+	}
+
+}
